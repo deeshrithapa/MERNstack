ed155651f6c2413d58bd5c00a45dd8332ba92d21
deeshrithapa <deeshri.t@gmail.com>
2024-05-20 19:13:16 +0545

    First

---
+++ Practical/js/lesson1.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+
+var fullName ="Deeshri Thapa"; //define
+console.log(fullName);
+
+// let can only  reassign  not redeclare 
+let age= 21;
+console.log(age);
+
+// no reassign and redeclare
+const role="Student";
+//  role = "Teacher";
+console.log(role);
+
+var lastName; // variable declaration 
+lastName = "Thapa" ;// assign
+console.log(lastName);
+
+var lastName = "Rai"; // redeclare 
+console.log(lastName);
+
+name = "lamb"; //reserved keyword 
+console.log(name);
+var name; 
